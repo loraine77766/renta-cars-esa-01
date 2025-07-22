ca80c89511c3e48da9e2ff36e9eea1be67ae4977
Edier Migadalia <ediermigadalia@gmail.com>
2025-07-22 11:14:41 +0000

    agrega un auto Hyundai Elantra     4 Puertas     5 Pasajeros     Autom

--- src/lib/cars.ts
+++ src/lib/cars.ts
@@ -3,6 +3,40 @@ import type { Car } from './types';
 
 export const cars: Car[] = [
   {
+    id: 11,
+    name: 'Hyundai Elantra',
+    description: 'Un sedán elegante y eficiente, perfecto para una conducción cómoda y con estilo.',
+    pricePerDay: 120,
+    originalPricePerDay: 134,
+    imageUrl: '/images/images/HyundaiElantra.jpg',
+    imageHint: 'sedan gray elegant',
+    features: ['4 Puertas', '5 Pasajeros', 'Automático', 'Gasolina', 'Clima', 'Maletero', 'Seguro incluido', 'Chapa ESA'],
+    details: {
+      additionalCharges: [
+        'Los cargos adicionales son abonados directamente a la compañía rentadora mediante tarjeta en el momento a la apertura del contrato. No se admitirán pagos en efectivo.'
+      ],
+      notIncluded: [
+        'Conductor adicional (Max. 2, precio por chofer por día: 3.50 USD).',
+        'Depósito de garantía.'
+      ],
+      included: [
+        'Seguro diario.',
+        'Depósito de Combustible.',
+        'Tasa de Aeropuerto (20.00 USD si aplica).'
+      ],
+      pickupAndDropoff: [
+        'Si su hora de DEVOLUCIÓN al momento de reservar es más de 4 horas que su hora de RECOGIDA el sistema cargara 1 día adicional a su reserva automáticamente. Asegúrese de entregar el auto a la hora establecida.'
+      ],
+      notes: [
+        'A conductores entre 21-24 años y 75-80 años de edad se le incrementará el costo del seguro en un 50%.',
+        'RentCubaCar solo confirma categorías, no marcas y/o modelos específicos.',
+        'Mínimo de renta: 5 días.',
+        'Costo de seguro por día: $40.00 USD.',
+        'Costo de combustible (única vez): $72.00 USD.'
+      ]
+    }
+  },
+  {
     id: 10,
     name: 'Dongfeng Shine Max',
     description: 'Un sedán premium, espacioso y con tecnología de punta para un viaje de lujo.',
